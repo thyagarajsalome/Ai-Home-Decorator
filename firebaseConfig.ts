56f5f715beb8e8f59a775cce83e593edbcc9dde6
https://github.com/thyagarajsalome/KQ-V1.0.0.git <thyagarajsalome@gmail.com>
2025-10-22 22:36:01 +0530

    updated

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 
 // TODO: Replace with your project's Firebase config object
 const firebaseConfig = {
-  apiKey: "AIza...",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "...",
-  appId: "1:...",
+  apiKey: "AIzaSyCeyA8wGYinpKBWqZwUO8UMd45GYVEsOM0",
+  authDomain: "aihomedecorator.firebaseapp.com",
+  projectId: "aihomedecorator",
+  storageBucket: "aihomedecorator.firebasestorage.app",
+  messagingSenderId: "358218923651",
+  appId: "1:358218923651:web:1242dc196f5a36ba0fa5b6",
+  measurementId: "G-1MTCQ2HXS6",
 };
 
 // Initialize Firebase
